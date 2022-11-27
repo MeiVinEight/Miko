cf2a3421ababd06d7bfc7dd526a36a00f1feb8ae
MeiVinEight <3390038158@qq.com>
2022-11-27 11:36:35 +0800

    Compression::NibbleReader -> EOF check

--- Compression/src/NibbleReader.cpp
+++ Compression/src/NibbleReader.cpp
@@ -8,21 +8,25 @@ QWORD Compression::NibbleReader::read(BYTE bits)
 {
 	if (bits <= 64)
 	{
-		QWORD ret = 0;
-		QWORD mov = 64;
-		while (bits)
+		if (this->position + bits <= this->stream.length * 8)
 		{
-			QWORD idx = this->position >> 3;
-			QWORD btx = this->position & 0x7;
+			QWORD ret = 0;
+			QWORD mov = 64;
+			while (bits)
+			{
+				QWORD idx = this->position >> 3;
+				QWORD btx = this->position & 0x7;
 
-			ret >>= 1;
-			ret |= ((this->stream[idx] >> btx) & 1ULL) << 63;
+				ret >>= 1;
+				ret |= ((this->stream[idx] >> btx) & 1ULL) << 63;
 
-			this->position++;
-			mov--;
-			bits--;
+				this->position++;
+				mov--;
+				bits--;
+			}
+			return ret >> mov;
 		}
-		return ret >> mov;
+		throw Exception::exception("EOF");
 	}
 	throw Exception::exception("Bitwise count is too large");
 }
