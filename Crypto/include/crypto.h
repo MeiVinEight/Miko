45881b19de4480902555f441a59efb1d2464f970
MeiVinEight <3390038158@qq.com>
2023-03-17 22:43:46 +0800

    Crypto -> CRC Algorithm

--- Crypto/include/crypto.h
+++ Crypto/include/crypto.h
@@ -16,6 +16,42 @@ namespace Crypto
 		CRYPTOAPI Memory::string encode(const Memory::string &);
 		CRYPTOAPI Memory::string decode(const Memory::string &);
 	}
+	class CRC
+	{
+		public:
+		static const BYTE CRC4_ITU          = 0x00;
+		static const BYTE CRC5_EPC          = 0x01;
+		static const BYTE CRC5_ITU          = 0x02;
+		static const BYTE CRC5_USB          = 0x03;
+		static const BYTE CRC6_ITU          = 0x04;
+		static const BYTE CRC7_MMC          = 0x05;
+		static const BYTE CRC8              = 0x06;
+		static const BYTE CRC8_ITU          = 0x07;
+		static const BYTE CRC8_ROCH         = 0x08;
+		static const BYTE CRC8_MAXIM        = 0x09;
+		static const BYTE CRC16_IBM         = 0x0A;
+		static const BYTE CRC16_MAXIM       = 0x0B;
+		static const BYTE CRC16_USB         = 0x0C;
+		static const BYTE CRC16_MODBUS      = 0x0D;
+		static const BYTE CRC16_CCITT       = 0x0E;
+		static const BYTE CRC16_CCITT_FALSE = 0x0F;
+		static const BYTE CRC16_X25         = 0x10;
+		static const BYTE CRC16_XMODEM      = 0x11;
+		static const BYTE CRC16_XMODEM2     = 0x12;
+		static const BYTE CRC16_DNP         = 0x13;
+		static const BYTE CRC32             = 0x14;
+		static const BYTE CRC32_C           = 0x15;
+		static const BYTE CRC32_KOOPMAN     = 0x16;
+		static const BYTE CRC32_MPEG_2      = 0x17;
+		static const BYTE CRC64_ISO         = 0x18;
+		static const BYTE CRC64_ECMA        = 0x19;
+		const BYTE type = 0;
+		QWORD crc = 0;
+
+		CRYPTOAPI CRC(BYTE);
+		CRYPTOAPI void update(const void *, QWORD);
+		CRYPTOAPI QWORD value() const;
+	};
 }
 
 #endif //CRYPTO_H
--- WST/src/main.cpp
+++ WST/src/main.cpp
@@ -4,10 +4,9 @@
 void func()
 {
 	String::string text = "https://github.com/MeiVinEight/Miko";
-	Streaming::cout << text << Streaming::LF;
-	Memory::string base64 = Crypto::BASE64::encode(text.address);
-	Streaming::cout << String::string(base64) << Streaming::LF;
-	Streaming::cout << String::string(Crypto::BASE64::decode(base64));
+	Crypto::CRC crc(Crypto::CRC::CRC64_ECMA);
+	crc.update(text.address.address, text.length());
+	Streaming::cout << crc.value();
 }
 
 int main()
