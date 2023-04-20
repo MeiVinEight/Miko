47916a1e08921df2e99610e43d19780cabb2f6d8
MeiVinEight <3390038158@qq.com>
2023-04-20 09:50:42 +0800

    Cryptography -> Simplify code

--- Cryptography/src/DllMain.cpp
+++ Cryptography/src/DllMain.cpp
@@ -9,13 +9,13 @@ int __stdcall DllMain(HINSTANCE *, unsigned int reason, void *)
 		{
 			static const WCHAR RNG[] = L"RNG";
 			NTSTATUS status = BCryptOpenAlgorithmProvider(&RNG_HANDLE, RNG, nullptr, 0);
-			result &= status == 0;
+			result &= !status;
 			break;
 		}
 		case DLL_PROCESS_DETACH:
 		{
 			NTSTATUS status = BCryptCloseAlgorithmProvider(&RNG_HANDLE, 0);
-			result &= status == 0;
+			result &= !status;
 			break;
 		}
 	}
