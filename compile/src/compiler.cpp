7817915e45a6a17dbd4d1206a218160e3bd1ee37
夏雨 <3390038158@qq.com>
2023-06-16 22:33:36 +0800

    compiler, configured with json file

--- compile/src/compiler.cpp
+++ compile/src/compiler.cpp
@@ -7,6 +7,7 @@
 /*-------------------------------- NT declarations --------------------------------*/
 #pragma region NTDECL
 #define MAX_PATH          260
+#define FILE_ATTRIBUTE_DIRECTORY	0x00000010
 
 extern "C"
 {
@@ -26,8 +27,8 @@ typedef struct _WIN32_FIND_DATAA
 	DWORD    nFileSizeLow;
 	DWORD    dwReserved0;
 	DWORD    dwReserved1;
-	char     cFileName[ MAX_PATH ];
-	char     cAlternateFileName[ 14 ];
+	char     cFileName[MAX_PATH];
+	char     cAlternateFileName[14];
 } WIN32_FIND_DATAA, *PWIN32_FIND_DATAA, *LPWIN32_FIND_DATAA;
 typedef struct _STARTUPINFOA
 {
@@ -71,13 +72,6 @@ BOOL __stdcall GetExitCodeProcess(void *, DWORD *);
 #pragma endregion NTDECL
 /*-------------------------------- NT declarations --------------------------------*/
 
-// placement new
-void *__cdecl operator new(unsigned long long, void *where) noexcept
-{
-	return where;
-}
-
-
 // project object
 class project
 {
@@ -87,8 +81,15 @@ class project
 	bool compiled = false;     // whether project has successfully built in current build task
 
 	project(String::string);
-	void prefix();
 	bool compile();
+	bool depend();
+	String::string include();
+	String::string resource();
+	String::string source();
+	String::string option(int);
+	String::string definitions();
+	String::string object();
+	String::string executable();
 };
 
 struct ProjectNode
@@ -123,8 +124,11 @@ String::string ReadJson(const String::string &filePath)
 	// Memory::string will automatically convert to String::string
 	return buf;
 }
-bool SourceFilter(const char *name)
+bool SourceFilter(WIN32_FIND_DATAA *data)
 {
+	if (data->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+		return false;
+	const char *name = data->cFileName;
 	QWORD length = String::length(name);
 	const char *ext = name + length;
 	while (ext >= name && *ext != '.')
@@ -136,10 +140,24 @@ bool SourceFilter(const char *name)
 		return false;
 	return *ext == 'C' || *ext == 'c';
 }
-// Foreach files in folder and concat them into a string
-String::string ForeachFiles(const String::string &folder, bool (*filter)(const char *))
+bool ResourceFilter(WIN32_FIND_DATAA *data)
 {
-	String::string files;
+	char ext[] = ".rc";
+	if (data->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+		return false;
+	QWORD length = String::length(data->cFileName);
+	if (length >= 3)
+	{
+		length -= 3;
+		return Memory::compare(ext, data->cFileName + length, 3);
+	}
+	return false;
+}
+// Foreach files in folder and concat them into a string
+JSON::object ForeachFiles(const String::string &folder, bool (*filter)(WIN32_FIND_DATAA *))
+{
+	JSON::object files;
+	QWORD idx = 0;
 	Memory::string cstr = (folder + "\\*").native();
 	WIN32_FIND_DATAA data = {};
 	void *handle = FindFirstFileA((const char *) cstr.address, &data);
@@ -148,12 +166,9 @@ String::string ForeachFiles(const String::string &folder, bool (*filter)(const c
 		bool search = true;
 		while (search)
 		{
-			if (filter(data.cFileName))
+			if (filter(&data))
 			{
-				files += folder;
-				files += "\\";
-				files += data.cFileName;
-				files += " ";
+				files[idx++] = (folder + "\\") + (const char *) data.cFileName;
 			}
 			data = {};
 			search = FindNextFileA(handle, &data);
@@ -213,18 +228,50 @@ void FreeList()
 		Memory::free(node);
 		node = next;
 	}
+	node = nullptr;
+}
+String::string ConcatStringArray(const JSON::object &obj)
+{
+	JSON::object &arr = (JSON::object &) obj;
+	String::string value;
+	if (arr.type == JSON::type::ARRAY)
+	{
+		for (QWORD i = 0; i < arr.length(); i++)
+		{
+			if (arr[i].type == JSON::type::STRING)
+			{
+				value += arr[i];
+				value += " ";
+			}
+		}
+	}
+	return value;
+}
+String::string EscapeString(const String::string &str)
+{
+	QWORD len = str.length();
+	for (QWORD i = 0; i < str.length(); len += str[i++] == '\\');
+	String::string retval;
+	retval.address.resize(len);
+	QWORD idx = 0;
+	for (QWORD i = 0; i < str.length(); i++)
+	{
+		retval[idx++] = str[i];
+		if (str[i] == '\\')
+		{
+			retval [idx++] = '\\';
+		}
+	}
+	return retval;
 }
 
 
-project::project(String::string path): path((String::string &&) path), config(JSON::resolve(ReadJson(this->path + "\\config.json")))
+project::project(String::string path):
+path((String::string &&) path),
+config(JSON::resolve(ReadJson(this->path + "\\config.json")))
 {
-	if (this->config.type != JSON::type::COMPONENT)
-	{
-		Streaming::cout << this->path << Streaming::LF;
-		Streaming::cout << "config file must be component" << Streaming::LF;
-		throw Memory::exception(JSON::ERRNO_WRONG_OBJECT_TYPE);
-	}
-	Streaming::cout << "\x1B[104m LOADING \x1B[0m ";
+	this->config.readonly = true;
+	Streaming::cout << "\x1B[104;97m LOADING \x1B[0m ";
 	Streaming::cout << ((String::string) this->config["name"]) << " [" << this->path << "]\n";
 	// Link this project into project list
 	// use header link
@@ -234,39 +281,24 @@ project::project(String::string path): path((String::string &&) path), config(JS
 	ProjectList = node;
 
 	// load subprojects if it has "subproject" config field
-	static String::string subprojKey = "subproject";
+	String::string subprojKey = "subproject";
 	if (this->config.contain(subprojKey))
 	{
 		JSON::object subprojects = this->config[subprojKey];
-		if (subprojects.type != JSON::type::ARRAY)
-		{
-			Streaming::cout << "subproject must be array" << Streaming::LF;
-			return;
-		}
 
 		for (QWORD i = 0; i < subprojects.length(); i++)
 		{
 			JSON::object subproj = subprojects[i];
-			if (subproj.type != JSON::type::STRING)
+
+			String::string subprojPath = this->path + "\\" + subproj;
+			if (Filesystem::absolute(subproj))
 			{
-				Streaming::cout << "subproject element must be string" << Streaming::LF;
+				subprojPath = subproj;
 			}
-			else
-			{
-				String::string subprojPath = this->path + "\\" + subproj;
-				if (Filesystem::absolute(subproj))
-				{
-					subprojPath = subproj;
-				}
-				project *proj = (project *) Memory::allocate(sizeof(project));
-				new (proj) project(subprojPath);
-			}
+			project *proj = (project *) Memory::allocate(sizeof(project));
+			new (proj) project(subprojPath);
 		}
 	}
-}
-void project::prefix()
-{
-	Streaming::cout << "[" << ((String::string &) this->config["name"]) << "]: ";
 }
 bool project::compile()
 {
@@ -274,75 +306,187 @@ bool project::compile()
 	if (this->compiled)
 		return true;
 
+	bool success = this->depend();
+	if (!success)
+		return false;
+
+
+	String::string sourceFiles = this->source();
+
+	String::string resourceFiles = this->resource();
+	// if project has source file(s), compile and link them
+	if (sourceFiles)
+	{
+		Streaming::cout << "\x1B[46;97m COMPILING \x1B[0m ";
+		Streaming::cout << ((String::string) this->config["name"]) << " [" << this->path << "]\n";
+
+		// default compiler is cl.exe
+		String::string cmd = "cl.exe ";
+		cmd += sourceFiles;
+		cmd += this->include();
+		cmd += this->definitions();
+		cmd += this->object();
+		cmd += this->executable();
+		cmd += this->option(0);
+		cmd += resourceFiles;
+		success &= ExecuteCommand(cmd, this->path);
+		Streaming::cout << Streaming::LF;
+		if (!success)
+			return false;
+	}
+	this->compiled = true;
+	return true;
+}
+bool project::depend()
+{
 	bool success = true;
 	// compile depends
-	static String::string dependsKey = "depends";
+	String::string dependsKey = "depends";
 	if (this->config.contain(dependsKey))
 	{
 		JSON::object &depends = this->config[dependsKey];
-		if (depends.type != JSON::type::ARRAY)
+
+		for (QWORD i = 0; success && i < depends.length(); i++)
 		{
-			this->prefix();
-			Streaming::cout << "depends must be array" << Streaming::LF;
-		}
-		else
-		{
-			for (QWORD i = 0; success && i < depends.size; i++)
+			JSON::object &depele = depends[i];
+
+			const String::string &depName = depele;
+			project *depproj = FindProject(depName);
+			if (!depproj)
 			{
-				JSON::object &depele = depends[i];
-				if (depele.type != JSON::type::STRING)
-				{
-					this->prefix();
-					Streaming::cout << "depends element must be string" << Streaming::LF;
-				}
-				else
-				{
-					const String::string &depName = depele;
-					project *depproj = FindProject(depName);
-					if (depproj)
-					{
-						success &= depproj->compile();
-					}
-					else
-					{
-						this->prefix();
-						Streaming::cout << "Cannot find project [" << depName << "]\n";
-					}
-				}
+				Streaming::cout << "Cannot find project [" << depName << "]\n";
+				return false;
 			}
+
+			success &= depproj->compile();
 		}
 	}
-	if (!success)
-		return false;
-	// String::string &name = this->config["name"];
+	return success;
+}
+String::string project::include()
+{
+	String::string retval;
+	// depends
+	String::string dependsKey = "depends";
+	if (this->config.contain(dependsKey))
+	{
+		JSON::object &depends = this->config[dependsKey];
+		for (QWORD i = 0; i < depends.length(); i++)
+		{
+			JSON::object &depele = depends[i];
+			const String::string &projname = depele;
+			project *proj = FindProject(projname);
+			retval += proj->include();
+		}
+	}
+
+	String::string includeKey = "include";
+	if (this->config.contain(includeKey))
+	{
+		JSON::object &include = this->config[includeKey];
+		if (include.type == JSON::type::STRING)
+		{
+			String::string dir = include;
+			if (!Filesystem::absolute(dir))
+			{
+				dir = Filesystem::canonicalize(this->path + "\\" + dir);
+			}
+			retval += "/I \"";
+			retval += EscapeString(dir);
+			retval += "\" ";
+		}
+		else if (include.type == JSON::type::ARRAY)
+		{
+			for (QWORD i = 0; i < include.length(); i++)
+			{
+				String::string dir = include[i];
+				if (!Filesystem::absolute(dir))
+				{
+					dir = Filesystem::canonicalize(this->path + "\\" + dir);
+				}
+				retval += "/I \"";
+				retval += EscapeString(dir);
+				retval += "\" ";
+			}
+		}
+		else throw Memory::exception(JSON::ERRNO_WRONG_OBJECT_TYPE);
+	}
+	return retval;
+}
+String::string project::resource()
+{
+	String::string resourceKey = "resource";
+	JSON::object rcArray;
+	if (!this->config.contain(resourceKey))
+	{
+		rcArray = ForeachFiles(this->path, ResourceFilter);
+	}
+	else
+	{
+		JSON::object &resource = this->config[resourceKey];
+		if (resource.type == JSON::type::STRING)
+		{
+			String::string folder = this->path + "\\" + resource;
+			if (Filesystem::absolute(resource))
+			{
+				folder = resource;
+			}
+			rcArray = ForeachFiles(folder, ResourceFilter);
+		}
+		else if (resource.type == JSON::type::ARRAY)
+		{
+			QWORD idx = 0;
+			for (QWORD i = 0; i < resource.length(); i++)
+			{
+				JSON::object &resele = resource[i];
+
+				String::string folder = this->path + "\\" + resele;
+				if (Filesystem::absolute(resele))
+				{
+					folder = resele;
+				}
+				JSON::object rcArr = ForeachFiles(folder, ResourceFilter);
+				for (QWORD j = 0; j < rcArr.length(); j++)
+				{
+					rcArray[idx + j] = rcArr[j];
+				}
+				idx += rcArr.length();
+			}
+		}
+		else throw Memory::exception(JSON::ERRNO_WRONG_OBJECT_TYPE);
+	}
+
+	String::string resFiles;
+	for (QWORD i = 0; i < rcArray.length(); i++)
+	{
+		if (!i)
+		{
+			Streaming::cout << "\x1B[46;97m RESOURCE \x1B[0m ";
+			Streaming::cout << ((String::string) this->config["name"]) << " [" << this->path << "]\n";
+		}
+		String::string input = rcArray[i];
+		String::string output = String::string(input.address, input.length() - 2) + "res";
+		String::string cmd = "rc.exe /FO\"";
+		cmd += output;
+		cmd += "\" ";
+		cmd += input;
+		if (ExecuteCommand(cmd, this->path))
+		{
+			Streaming::cout << input << " -> " << output << Streaming::LF;
+			resFiles += output;
+			resFiles += " ";
+		}
+	}
+	return resFiles;
+}
+String::string project::source()
+{
 	String::string sourceKey = "source";
-
-	// default compiler is cl.exe
-	String::string clr = "cl.exe ";
-
-
-
 	String::string sourceFiles;
 	if (!this->config.contain(sourceKey))
 	{
-		// Not source configured, use project path as source folder
-		sourceFiles = ForeachFiles(this->path, SourceFilter);
-		/*
-		Memory::string cstr = this->path.native();
-		WIN32_FIND_DATAA data = {};
-		void *handle = FindFirstFileA((const char *) cstr.address, &data);
-		if (handle != ((void *) -1))
-		{
-			bool search = true;
-			while (search)
-			{
-				sourceFiles += data.cFileName;
-				sourceFiles += " ";
-				search = FindNextFileA(handle, &data);
-			}
-			FindClose(handle);
-		}
-		*/
+		// No source configured, use project path as source folder
+		sourceFiles = ConcatStringArray(ForeachFiles(this->path, SourceFilter));
 	}
 	else/* if (this->config[sourceKey].type != JSON::type::UNKNOWN)*/
 	{
@@ -356,82 +500,109 @@ bool project::compile()
 			{
 				folder = source;
 			}
-			sourceFiles = ForeachFiles(folder, SourceFilter);
+			sourceFiles = ConcatStringArray(ForeachFiles(folder, SourceFilter));
 		}
-		else if (source.type == JSON::type::ARRAY)
+		else if (source.type == JSON::ARRAY)
 		{
 			// sources are in an array in config
-			for (QWORD i = 0; i < source.size; i++)
+			for (QWORD i = 0; i < source.length(); i++)
 			{
 				JSON::object &sourceEle = source[i];
-				if (sourceEle.type != JSON::type::STRING)
+
+				String::string folder = this->path + "\\" + sourceEle;
+				if (Filesystem::absolute(sourceEle))
 				{
-					this->prefix();
-					Streaming::cout << "source element must be string" << Streaming::LF;
+					folder = sourceEle;
 				}
-				else
-				{
-					String::string folder = this->path + "\\" + sourceEle;
-					if (Filesystem::absolute(sourceEle))
-					{
-						folder = sourceEle;
-					}
-					sourceFiles += ForeachFiles(folder, SourceFilter);
-				}
+				sourceFiles += ConcatStringArray(ForeachFiles(folder, SourceFilter));
 			}
 		}
-		else if (source.type != JSON::type::UNKNOWN)
-		{
-			// configured source bug unrecognized
-			this->prefix();
-			Streaming::cout << "Connot find source folders" << Streaming::LF;
-			return false;
-		}
+		else if (source.type != JSON::UNKNOWN) throw Memory::exception(JSON::ERRNO_WRONG_OBJECT_TYPE);
 	}
-
-	// if project has source file(s), compile and link them
-	if (sourceFiles)
+	return sourceFiles;
+}
+String::string project::option(int type)
+{
+	// opt type, 0 is compiler options, 1 is linker options
+	String::string optionsType[] = {"compiler", "linker"};
+	String::string optionsKey = "options";
+	JSON::object *array = nullptr;
+	if (this->config.contain(optionsKey))
 	{
-		String::string clropt;
-		String::string optionKey = "option";
-		// check compiler option
-		// $.option
-		if (this->config.contain(optionKey))
+		JSON::object &options = this->config[optionsKey];
+		if (options.type == JSON::type::COMPONENT)
 		{
-			JSON::object &options = this->config[optionKey];
-			if (options.type != JSON::type::ARRAY)
+			// "options": {"compiler": [], "linker": []}
+			if (options.contain(optionsType[type]))
 			{
-				this->prefix();
-				Streaming::cout << "option must be array" << Streaming::LF;
-			}
-			else
-			{
-				for (QWORD i = 0; i < options.length(); i++)
+				JSON::object &optArr = options[optionsType[type]];
+				if (optArr.type == JSON::type::ARRAY)
 				{
-					if (options[i].type != JSON::type::STRING)
-					{
-						this->prefix();
-						Streaming::cout << "option element must be string" << Streaming::LF;
-					}
-					else
-					{
-						clropt += options[i];
-						clropt += " ";
-					}
+					array = &optArr;
 				}
 			}
 		}
-
-
-		Streaming::cout << "\x1B[46m COMPILING \x1B[0m ";
-		Streaming::cout << ((String::string) this->config["name"]) << " [" << this->path << "]\n";
-		success &= ExecuteCommand(clr + sourceFiles + clropt, this->path);
-		Streaming::cout << Streaming::LF;
-		if (!success)
-			return false;
+		else if (options.type == JSON::type::ARRAY)
+		{
+			// "options": []
+			// default is compiler options
+			if (type == 0)
+			{
+				array = &options;
+			}
+		}
+		else throw Memory::exception(JSON::ERRNO_WRONG_OBJECT_TYPE);
 	}
-	this->compiled = true;
-	return true;
+
+	String::string optionsValue;
+	if (array)
+	{
+		optionsValue = ConcatStringArray(*array);
+	}
+	return optionsValue;
+}
+String::string project::definitions()
+{
+	String::string definitionsKey = "definitions";
+	if (this->config.contain(definitionsKey))
+	{
+		JSON::object &definitions = this->config[definitionsKey];
+		String::string retval;
+		for (QWORD i = 0; i < definitions.length(); i++)
+		{
+			JSON::object &element = definitions[i];
+			retval += "/D ";
+			retval += (String::string) element;
+			retval += " ";
+		}
+		return retval;
+	}
+	return "";
+}
+// TODO this two function is too similar
+String::string project::object()
+{
+	String::string objKey = "obj";
+	if (this->config.contain(objKey))
+	{
+		JSON::object &obj = this->config[objKey];
+		String::string fo = "/Fo\"";
+		String::string pathname = EscapeString(obj);
+		return fo + pathname + "\" ";
+	}
+	return "";
+}
+String::string project::executable()
+{
+	String::string exeKey = "exe";
+	if (this->config.contain(exeKey))
+	{
+		JSON::object &exe = this->config[exeKey];
+		String::string fe = "/Fe:\"";
+		String::string pathname = EscapeString(exe);
+		return fe + pathname + "\" ";
+	}
+	return "";
 }
 
 int main()
@@ -447,25 +618,50 @@ int main()
 
 		// Foreach project list and compile
 		ProjectNode *node = ProjectList;
-		while (node)
+		while (success && node)
 		{
 			success &= node->project->compile();
 			node = node->next;
 		}
-
-		FreeList();
 	}
 	catch (Memory::exception &exce)
 	{
-		Streaming::cout << exce.message << Streaming::LF;
 		success = false;
-		try
+		Streaming::cout << exce.code << ": " << exce.message << Streaming::LF;
+		for (QWORD i = 0; i < exce.count; i++)
 		{
-			FreeList();
+			Streaming::cout << exce.stack[i].offset << " (";
+			if (exce.stack[i].function.length)
+			{
+				Streaming::cout << exce.stack[i].function << '+';
+				Streaming::cout << Hexadecimal::stringify((QWORD) exce.stack[i].offset - (QWORD) exce.stack[i].address);
+			}
+			else
+			{
+				Streaming::cout << exce.stack[i].offset;
+			}
+			Streaming::cout << ") [";
+			if (exce.stack[i].module)
+			{
+				Streaming::cout << exce.stack[i].library << '+';
+				Streaming::cout << Hexadecimal::stringify((QWORD) exce.stack[i].offset - (QWORD) exce.stack[i].module);
+			}
+			else
+			{
+				Streaming::cout << exce.stack[i].address;
+			}
+			Streaming::cout << ']' << Streaming::LF;
 		}
-		catch (...)
-		{
-		}
+	}
+	catch (...)
+	{
+		success = false;
+		Streaming::cout << "Unknown internal error" << Streaming::LF;
+	}
+
+	try
+	{
+		FreeList();
 	}
 	catch (...)
 	{
@@ -473,11 +669,11 @@ int main()
 
 	if (success)
 	{
-		Streaming::cout << "\x1B[42m SUCCESSFUL \x1B[0m";
+		Streaming::cout << "\x1B[42;97m SUCCESSFUL \x1B[0m";
 	}
 	else
 	{
-		Streaming::cout << "\x1B[41m FAILED \x1B[0m";
+		Streaming::cout << "\x1B[41;97m FAILED \x1B[0m";
 	}
 	return 0;
 }
