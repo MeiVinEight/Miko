b4637be93be494aa6c121aa21ec24ae7e90e9454
夏雨 <3390038158@qq.com>
2023-06-17 20:38:07 +0800

    [JSON] global variable initializer

---
+++ JSON/src/DllMain.c
@@ -0,0 +1,42 @@
+#ifndef CMAKE_BUILD
+
+#define DLL_PROCESS_DETACH 0
+#define DLL_PROCESS_ATTACH 1
+
+#pragma section(".CRT$XCA", long, read) // First C++ Initializer
+#pragma section(".CRT$XCZ", long, read) // Last C++ Initializer
+
+__declspec(allocate(".CRT$XCA")) void (__cdecl *XCA)(void) = 0;
+__declspec(allocate(".CRT$XCZ")) void (__cdecl *XCZ)(void) = 0;
+
+#pragma comment(linker, "/MERGE:.CRT=.rdata")
+
+void CRTINIT()
+{
+	void (__cdecl **xca)(void) = &XCA;
+	while (xca < &XCZ)
+	{
+		if (*xca)
+		{
+			(*xca)();
+		}
+		xca++;
+	}
+}
+
+int _DllMainCRTStartup(void *instance, unsigned int reason, void *reserved)
+{
+	(void) instance;
+	(void) reserved;
+	switch (reason)
+	{
+		case DLL_PROCESS_ATTACH:
+		{
+			CRTINIT();
+			break;
+		}
+	}
+	return 1;
+}
+
+#endif
