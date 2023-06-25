231f22af2c8af3d6a9e6e1819bebc1bef1d9e198
夏雨 <3390038158@qq.com>
2023-06-25 08:56:58 +0800

    [WST] Test code

--- WST/src/main.cpp
+++ WST/src/main.cpp
@@ -1,27 +1,10 @@
-#include <sstring.h>
+#include <exception.h>
+#include <standardstream.h>
+#include <hexadecimal.h>
 #include <streaming.h>
-#include <filesystem.h>
-#include <WTM.h>
-
-DWORD ThreadProc(void *param)
-{
-	bool *flag = (bool *) param;
-	while (*flag)
-	{
-		WTM::thread::sleep(1);
-	}
-	WTM::thread curr = WTM::thread::current();
-	Streaming::cout << curr.name() << Streaming::LF;
-	return 12138;
-}
 
 void func()
 {
-	bool flag = true;
-	WTM::thread thread = WTM::thread::create(ThreadProc, &flag);
-	thread.name((String::string("Sample text")).address);
-	flag = false;
-	thread.wait();
 }
 
 int main()
@@ -35,13 +18,6 @@ int main()
 		Streaming::cout << exec.code << ": " << exec.message << Streaming::LF;
 		for (DWORD i = 0; i < exec.count; i++)
 		{
-			/*
-			Streaming::cout << exec.stack[i].address << ' ';
-			Streaming::cout << exec.stack[i].offset << ' ';
-			Streaming::cout << exec.stack[i].module << ' ';
-			Streaming::cout << exec.stack[i].function << ' ';
-			Streaming::cout << exec.stack[i].library << Streaming::LF;
-			*/
 			Streaming::cout << exec.stack[i].offset << " (";
 			if (exec.stack[i].function.length)
 			{
@@ -53,15 +29,8 @@ int main()
 				Streaming::cout << exec.stack[i].offset;
 			}
 			Streaming::cout << ") [";
-			if (exec.stack[i].module)
-			{
-				Streaming::cout << exec.stack[i].library << '+';
-				Streaming::cout << Hexadecimal::stringify(((QWORD) exec.stack[i].offset) - ((QWORD) exec.stack[i].module));
-			}
-			else
-			{
-				Streaming::cout << exec.stack[i].address;
-			}
+			Streaming::cout << exec.stack[i].library << '+';
+			Streaming::cout << Hexadecimal::stringify(((QWORD) exec.stack[i].offset) - ((QWORD) exec.stack[i].module));
 			Streaming::cout << ']' << Streaming::LF;
 		}
 	}
